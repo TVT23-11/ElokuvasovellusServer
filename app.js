65d055e10ebc60f0dec955124f1afae1a1b914e6
DapiFromKazzak <aaro.kallioinen@gmail.com>
2024-04-06 22:13:12 +0300

    Käyttäjätilin luomiseen tehty backend

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const dotenv = require('dotenv').config();
 var express = require('express');
 
-const https = require('https');
-const pgPool = require('./database/pg_connection');
 const TMDB_router = require('./controllers/TMDBController');
+const User_router = require('./controllers/userController');
 
 const App=express();
 const cors = require('cors');
-//App.use(express.json);
-
-App.use(express.static('public'));
 App.use(cors());
 
-App.get('/', () => {
-    let data = '';
-    const request = https.get('https://api.themoviedb.org/3/movie/11?api_key=' + process.env.API_KEY, (response) => {
-        console.log('https://api.themoviedb.org/3/movie/11?api_key=' + process.env.API_KEY);
-        response.on('data', (chunk) => {
-            data += chunk;
-            console.log(response.data);
-        });
-        response.on('end', () => {
-            console.log("error");
-            console.log(data);
-        });
-        request.end();
-    });
-});
-
 App.use('/TMDB', TMDB_router);
+App.use('/user', User_router);
 
-App.listen(process.env.PORT, function(){
+App.listen(process.env.PORT, function(err){
+    if (err) console.log(err);
     console.log("listening port " + process.env.PORT);
 });
 
