ad8ddfa6b1289e8f1fe00a45c76f35b5f04217ad
DapiFromKazzak <aaro.kallioinen@gmail.com>
2024-04-15 00:17:04 +0300

    Kirjautumisessa saatu jwt token tallennetaan Session Storageen token muuttujaan

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,83 +5,92 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv').config();
 
-    // Uuden käyttäjän lisäys
+// Uuden käyttäjän lisäys
 Router.post('/register',
-    function (request, response) {
+    async function (request, response) {
         User.add(request.body, function (err, dbResult) {
             if (err) {
                 response.json(err);
                 console.log(err);
             } else {
-                //console.log("add");
-                //console.log(dbResult);
                 response.status(200).json("ok");
                 response.end();
             }
         });
     });
+
 Router.post('/login',
-    function(request, response){
-        if(request.body.username && request.body.password){
+    function (request, response) {
+        if (request.body.username && request.body.password) {
             const user = request.body.username;
             const pass = request.body.password;
 
-            User.checkPassword(user, function(err, dbResult){
-                if(err){
+            User.checkPassword(user, function (err, dbResult) {
+                if (err) {
                     response.json(err);
                 } else {
                     console.log(dbResult.rows[0].password);
-                    
-                    bcrypt.compare(pass, dbResult.rows[0].password, function(err,compareResult) {
-                        if(compareResult) {
-                          console.log("succes");
-                          const token = generateAccessToken({ username: user });
-                          response.json(token);
-                          //response.send(token);
-                        }
-                        else {
-                            console.log("wrong password");
-                            response.json('wrong password');
-                            //response.send(false);
-                        }			
-                    });
-                    
+                    if (dbResult.rowCount > 0) {    // Käyttäjätili löytyi                       
+                        bcrypt.compare(pass, dbResult.rows[0].password, async function (err, compareResult) {
+                            if (err) {
+                                console.log(err);
+                                json.status(404).json({ error: err });
+                            } else {
+                                if (compareResult) {
+                                    console.log("succes");
+                                    //const token = generateAccessToken({ username: user });
+                                    const token = await jwt.sign({ username: user }, process.env.JWT_SECRET_KEY);
+                                    response.status(200).json({ jwtToken: token });
+                                } else {
+                                    console.log("wrong password");
+                                    response.status(401).json({ error: 'wrong password' });
+                                }
+                            }
+                        });
+                    } else {
+                        console.log('Käyttäjätunnusta ei löytynyt tietokannasta');
+                        response.status(401).json({ error: 'wrong password' });
+                    }
                 }
             });
+        } else {
+            console.log('Käyttäjätunnusta tai salasanaa ei löytynyt post-datasta');
+            response.status(404).json({ error: 'wrong password' });
         }
+        //response.end();
     });
 
-    // Tarkistaa, onko käyttäjätunnus varattu
+// Tarkistaa, onko käyttäjätunnus varattu
 Router.get('/checkUsername',
     function (request, response) {
         console.log("checking username " + request.query.username);
-        User.isUsernameAvailable(request.query.username, function (err, dbResult){
-            if(err){
+        User.isUsernameAvailable(request.query.username, function (err, dbResult) {
+            if (err) {
                 response.json(err);
             } else {
-                if(dbResult.rows.length > 0){
+                if (dbResult.rows.length > 0) {
                     response.json('not available');
                 } else {
                     response.json('available');
                 }
-                
+
             }
         });
     });
 
-    //Tarkistaa, onko sähköpostilla jo luotu tili
+//Tarkistaa, onko sähköpostilla jo luotu tili
 Router.get('/checkEmail',
     function (request, response) {
-        User.isEmailAvailable(request.query.email, function (err, dbResult){
-            if(err){
+        User.isEmailAvailable(request.query.email, function (err, dbResult) {
+            if (err) {
                 response.json(err);
             } else {
-                if(dbResult.rows.length > 0){
+                if (dbResult.rows.length > 0) {
                     response.json('not available');
                 } else {
                     response.json('available');
                 }
-                
+
             }
         });
     });
